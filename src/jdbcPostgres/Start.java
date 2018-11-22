a5482237172bb8c33b2afb324723ea49787c585e
Thilo Graffe <thilo-graffe@web.de>
2018-11-22 11:17:28 +0100

    Project initialized.
    Added create table, drop table.

--- src/jdbcPostgres/Start.java
+++ src/jdbcPostgres/Start.java
@@ -3,6 +3,7 @@ package jdbcPostgres;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,16 +13,84 @@ public class Start {
 	public static void main(String[] args) throws SQLException {
 		Start start = new Start();
 		start.connect();
-		start.createSQLPreparedStatement();
-		start.con.close();
+		start.dropTables();
+		start.createTables();
+		start.disconnect();
 	}
 	
 	public void connect() {
 		try {
-			con = DriverManager.getConnection("jdbc:postgresql:fh_dbi", "dbi", "dbi");
-			System.out.println("Verbunden!\n");
+			con = DriverManager.getConnection("jdbc:postgresql:dbi_p7", "dbi", "dbi");
+			System.out.println("Verbunden!");
 		}
 		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void disconnect() {
+		try {
+			con.close();
+			System.out.println("Disconnected!");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void createTables() {
+		try {
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate("create table branches\r\n" + 
+					"( branchid int not null,\r\n" + 
+					" branchname char(20) not null,\r\n" + 
+					" balance int not null,\r\n" + 
+					" address char(72) not null,\r\n" + 
+					" primary key (branchid) );\r\n" + 
+					"create table accounts\r\n" + 
+					"( accid int not null,\r\n" + 
+					" name char(20) not null,\r\n" + 
+					" balance int not null,\r\n" + 
+					"branchid int not null,\r\n" + 
+					"address char(68) not null,\r\n" + 
+					"primary key (accid),\r\n" + 
+					"foreign key (branchid) references branches ); " +
+					"create table tellers\r\n" + 
+					"( tellerid int not null,\r\n" + 
+					" tellername char(20) not null,\r\n" + 
+					" balance int not null,\r\n" + 
+					" branchid int not null,\r\n" + 
+					" address char(68) not null,\r\n" + 
+					" primary key (tellerid),\r\n" + 
+					" foreign key (branchid) references branches );\r\n" + 
+					"create table history\r\n" + 
+					"( accid int not null,\r\n" + 
+					" tellerid int not null,\r\n" + 
+					" delta int not null,\r\n" + 
+					" branchid int not null,\r\n" + 
+					" accbalance int not null,\r\n" + 
+					" cmmnt char(30) not null,\r\n" + 
+					" foreign key (accid) references accounts,\r\n" + 
+					" foreign key (tellerid) references tellers,\r\n" + 
+					" foreign key (branchid) references branches ); ");
+		
+			System.out.println("tables erstellt!");
+			stmt.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void dropTables() {
+		try {
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate("drop table if exists history, accounts, branches, tellers;");
+		
+			System.out.println("gedroppped!");
+			stmt.close();
+			
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
