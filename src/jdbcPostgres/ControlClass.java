af61ea5854122f161d0019233da851c86c49ea4d
Hendrik Klein-Hitpass <Henni.kleinh@gmail.com>
2018-12-04 20:10:39 +0100

    multishredding implemented

---
+++ src/jdbcPostgres/ControlClass.java
@@ -0,0 +1,47 @@
+package jdbcPostgres;
+
+import java.util.Scanner;
+
+public class ControlClass {
+	final static String DIGITS="0123456789";
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Herzlich willkommen zum Control-Panel des Datenbank-Systems der Gruppe dbi32!\n"
+							+ "Möchten sie die Datenbank reseten oder befüllen? (Geben sie 0 für Ersteres oder 1 für letzteres ein!)");
+		String str1 = scan.nextLine();
+		if(stringtoint(str1)<=1&&stringtoint(str1)>=0) {
+			if(stringtoint(str1)==0) {
+				Start task=new Start(0,0,0,0,0);
+				task.initialize();
+			}
+			else {
+				System.out.println("Wie hoch soll der Skalierungsfaktor sein?)");
+				String str2 = scan.nextLine();
+				System.out.println("Wie groß soll die Batchgröße sein?)");
+				String str3 = scan.nextLine();
+				System.out.println("Wie viele Threads?)");
+				String str4 = scan.nextLine();
+				for(int i=0;i<stringtoint(str4);i++) {
+					new Thread(new Start(1,stringtoint(str2),stringtoint(str3),stringtoint(str4),i)).start();
+					System.out.println("Thread "+i+" ist gestartet.");
+				}
+				
+			}
+		}
+		else {
+			System.out.println("Das ist weder eine 0 noch eine 1! Tschüss!");
+		}
+		System.out.println("Ende");
+		scan.close();
+	}
+	private static int stringtoint(String strin) {
+		int j=0;
+		for(int i=0;i<strin.length();i++) {
+			j=j*10;
+			j=j+DIGITS.indexOf(strin.charAt(i));
+		}
+		return j;
+	}
+
+}
--- src/jdbcPostgres/Start.java
+++ src/jdbcPostgres/Start.java
@@ -6,19 +6,75 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 
-public class Start {
-	static Connection con;
-	static final int scaleAcc = 100000;
-	static final int scaleTel = 10;
-	static final int batchSize = 10000;
+public class Start implements Runnable{
+	private Connection con;
+	static final int SCALEACC = 100000;	 //Faktor für die Tabelle accounts
+	static final int SCALETEL = 10;		 //Faktor für die Tabelle tellers
 	
-	//Mode wird auf 0 gesetzt um die Datenbank neu zu erzeugen und gegebenenfalls vorher zu löschen. Bei 1 wird die ntps-Datenbank gefüllt.
-	static final int mode = 	1;
+	//Skalierungsfaktor
+	private int n = 10;
 	
-	//Anzahl ist unser Skalierungsfaktor und wird bei der Methode insertIntoNtpsDatabase(anzahl) als parameter übergeben.
-	static final int anzahl = 	50;
+	//Größe der Bündel
+	private int batchSize = 10000;
 	
-	public static void main(String[] args) throws SQLException {
+	//Anzahl Threads
+	private int threadCount= 4;
+	
+	//Anzahl Threads
+	private int threadIndex= 0;
+	
+	Start(int mode, int anzahl, int pbatchSize, int anzahlThreads, int pthreadIndex){
+		n=anzahl;
+		batchSize=pbatchSize;
+		threadCount=anzahlThreads;
+		threadIndex=pthreadIndex;
+		
+		/*//mode==0 ist Modus für das Löschen und Erstellen der Tabellen.
+		if(mode==0) {
+			connect();
+			dropTables();
+			createTables();
+			//Table triggers werden ausgeschaltet
+			try {
+				PreparedStatement stmt = con.prepareStatement(
+					"ALTER TABLE accounts DISABLE TRIGGER ALL;\r\n" + 
+					"ALTER TABLE branches DISABLE TRIGGER ALL;\r\n" + 
+					"ALTER TABLE tellers DISABLE TRIGGER ALL;");
+				stmt.executeUpdate();
+				stmt.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}*/
+	}
+	public void initialize() {
+		connect();
+		dropTables();
+		createTables();
+		//Table triggers werden ausgeschaltet
+		try {
+			PreparedStatement stmt = con.prepareStatement(
+				"ALTER TABLE accounts DISABLE TRIGGER ALL;\r\n" + 
+				"ALTER TABLE branches DISABLE TRIGGER ALL;\r\n" + 
+				"ALTER TABLE tellers DISABLE TRIGGER ALL;");
+			stmt.executeUpdate();
+			stmt.close();
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		disconnect();
+	}
+	
+	public void run(){
+		System.out.println("Thread "+threadIndex+" ist wirklich gestartet.");
+		connect();
+		insertIntoNtpsDatabase();
+		disconnect();
+	}
+	
+	/*public void main(String[] args) throws SQLException {
 		connect();
 		
 		//mode==0 : Das ist der Modus für das Löschen und Erstellen der Tabellen.
@@ -32,9 +88,10 @@ public class Start {
 		}
 		disconnect();
 	}
+	*/
 	
 	//Verbindungsaufbau mit dem Datenbankserver.
-	public static void connect() {
+	public void connect() {
 		try {
 			con = DriverManager.getConnection("jdbc:postgresql:postgres", "postgres", "datenbank");
 			con.setAutoCommit(false);
@@ -46,7 +103,7 @@ public class Start {
 	}
 	
 	//Verbindungsabbruch mit dem Datenbankserver.
-	public static void disconnect() {
+	public void disconnect() {
 		try {
 			con.close();
 			System.out.println("Disconnected!");
@@ -57,7 +114,7 @@ public class Start {
 	}
 	
 	//Inizialisierung der Datenbank. Tabellen werden angelegt.
-	public static void createTables() {
+	public void createTables() {
 		try {
 			Statement stmt = con.createStatement();
 			stmt.executeUpdate("create table branches\r\n" + 
@@ -102,7 +159,7 @@ public class Start {
 	}
 	
 	//Löschen der Datenbank. Bereits existierende Tabellen werden gelöscht. 
-	public static void dropTables() {
+	public void dropTables() {
 		try {
 			Statement stmt = con.createStatement();
 			stmt.executeUpdate("drop table if exists history, accounts, branches, tellers;");
@@ -116,39 +173,32 @@ public class Start {
 	}
 	
 	//ntps-Datenbank wird auf dem Datenbankmanagmentsystem erzeugt. Der Skalierungsfaktor wird als Parameter übergeben.
-	public static void insertIntoNtpsDatabase(int n) {
+	public void insertIntoNtpsDatabase() {
 		long start = System.currentTimeMillis();
 		try {
-			//Table triggers werden ausgeschaltet
-			PreparedStatement stmt = con.prepareStatement(
-				"ALTER TABLE accounts DISABLE TRIGGER ALL;\r\n" + 
-				"ALTER TABLE branches DISABLE TRIGGER ALL;\r\n" + 
-				"ALTER TABLE tellers DISABLE TRIGGER ALL;");
-			stmt.executeUpdate();
-			stmt.close();
-
-			//Tabelle branches wird gefüllt.
-			stmt = con.prepareStatement(
-					"insert into branches values (?,?,?,?)");
+			PreparedStatement stmt=null;
+			if(threadIndex==0) {
+				//Tabelle branches wird gefüllt.
+				stmt = con.prepareStatement("insert into branches values (?,?,?,?)");
 			
-			for(int i = 1; i <= n; i++) {
-				stmt.setInt(1, i);
-				stmt.setString(2, "AutomobileAutomobile");
-				stmt.setInt(3, 0);
-				stmt.setString(4, "jlollduvxjffonasgwrnwhwmejokonginaobpcuyfyboquqqgknqjtllvewiheodziqjkrkn");
-				stmt.addBatch();
+				for(int i = 1; i <= n; i++) {
+					stmt.setInt(1, i);
+					stmt.setString(2, "AutomobileAutomobile");
+					stmt.setInt(3, 0);
+					stmt.setString(4, "jlollduvxjffonasgwrnwhwmejokonginaobpcuyfyboquqqgknqjtllvewiheodziqjkrkn");
+					stmt.addBatch();
+				}
+				//Da in die Tabelle branches nicht mehr Tupel eingefügt werden als unsere willkürliche batchSize groß ist, wird nur ein Batch erstellt.
+				stmt.executeBatch();
+				stmt.close();
 			}
-			//Da in die Tabelle branches nicht mehr Tupel eingefügt werden als unsere willkürliche batchSize groß ist, wird nur ein Batch erstellt.
-			stmt.executeBatch();
-			stmt.close();
-			
 			//Tabelle accounts wird gefüllt.
 			stmt = con.prepareStatement(
 					"insert into accounts values (?, ?, ?, ?, ?)");
 			
-			for(int i = 0; i < (scaleAcc*n)/batchSize; i++) {
+			for(int i = 0; i < (SCALEACC*n)/threadCount/batchSize; i++) {
 				for(int j=0;j<batchSize;j++) {
-					stmt.setInt(1, i*batchSize+j+1);
+					stmt.setInt(1, i*batchSize+j+(threadIndex*(SCALEACC*n/threadCount))+1);
 					stmt.setString(2, "AutomobileAutomobile");
 					stmt.setInt(3, 0);
 					stmt.setInt(4, (int)(Math.random()*n)+1);
@@ -158,32 +208,32 @@ public class Start {
 				stmt.executeBatch();
 			}
 			stmt.close();
-			
-			//Tabelle tellers wird gefüllt.
-			stmt = con.prepareStatement(
-					"insert into tellers values (?, ?, ?, ?, ?)");
-			
-			for(int i = 1; i <= scaleTel *n; i++) {
-				stmt.setInt(1, i);
-				stmt.setString(2, "AutomobileAutomobile");
-				stmt.setInt(3, 0);
-				stmt.setInt(4, (int)(Math.random()*n)+1);
-				stmt.setString(5, "lduvxjffonasgwrnwhwmejokonginaobpcuyfyboquqqgknqjtllvewiheodziqjkrkn");
-				stmt.addBatch();
-			}
-			//Da in die Tabelle branches nicht mehr Tupel eingefügt werden als unsere willkürliche batchSize groß ist, wird nur ein Batch erstellt.
-			stmt.executeBatch();
-			stmt.close();
-			
 			//Tabelle history wird nicht gefüllt.
+			if(threadIndex==0) {
+				//Tabelle tellers wird gefüllt.
+				stmt = con.prepareStatement("insert into tellers values (?, ?, ?, ?, ?)");
 			
-			//Table triggers werden wieder eingeschaltet
-			stmt = con.prepareStatement(
+				for(int i = 1; i <= SCALETEL *n; i++) {
+					stmt.setInt(1, i);
+					stmt.setString(2, "AutomobileAutomobile");
+					stmt.setInt(3, 0);
+					stmt.setInt(4, (int)(Math.random()*n)+1);
+					stmt.setString(5, "lduvxjffonasgwrnwhwmejokonginaobpcuyfyboquqqgknqjtllvewiheodziqjkrkn");
+					stmt.addBatch();
+				}
+				//Da in die Tabelle branches nicht mehr Tupel eingefügt werden als unsere willkürliche batchSize groß ist, wird nur ein Batch erstellt.
+				stmt.executeBatch();
+				stmt.close();
+			
+				//Table triggers werden wieder eingeschaltet
+				/*stmt = con.prepareStatement(
 					"ALTER TABLE accounts ENABLE TRIGGER ALL;\r\n" + 
 					"ALTER TABLE branches ENABLE TRIGGER ALL;\r\n" + 
 					"ALTER TABLE tellers ENABLE TRIGGER ALL;");
-			stmt.executeUpdate();
-			stmt.close();
+				stmt.executeUpdate();
+				stmt.close();
+				*/
+			}
 				
 			//Es wird einmal final ein commit ausgeführt.
 			con.commit();
